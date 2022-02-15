1fb21c000ac7ae4fc11330530363d07cfa6b5bd2
Sawer-Grin <68555428+Sawer-Grin@users.noreply.github.com>
2022-02-15 14:53:33 +0300

    Update laba1.sh

--- laba1.sh
+++ laba1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 for i in $( find "$1" -type d); do
-	amountSize=$( du -h --max-depth=0 "$i" | cut -f1 )
-	amountFiles=$( ls "$i" | wc -l )
+	mountainside=$( du -h --max-depth=0 "$i" | cut -f1 )
+	amountFiles=$( ls "$i" -F | grep -v / | wc -l )
 	echo "Directory: $i Size: $amountSize Value of files:$amountFiles" >> "$2"
 
 done
