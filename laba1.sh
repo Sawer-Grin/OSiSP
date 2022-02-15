f40e2ac49278ffa19c40e764425e131c065b8490
Sawer-Grin <68555428+Sawer-Grin@users.noreply.github.com>
2022-02-15 17:20:24 +0300

    Update laba1.sh

--- laba1.sh
+++ laba1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for i in $( find "$1" -type d); do
-	amountSize=$( du -h --max-depth=0 "$i" | cut -f1 )
+	amountSize=$( du --max-depth=0 "$i" | cut -f1 )
 	amountFiles=$( find "$i" -maxdepth 1 -type f | wc -l )
 	echo "Directory: $i Size: $amountSize Value of files:$amountFiles" >> "$2"
 
