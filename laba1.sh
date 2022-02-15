362e53388b46b50dac9571380ae53f0b5ee0d837
Sawer-Grin <68555428+Sawer-Grin@users.noreply.github.com>
2022-02-15 13:02:52 +0300

    Add files via upload

--- laba1.sh
+++ laba1.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-
 for i in $( find "$1" -type d); do
 	amountSize=$( du -h --max-depth=0 "$i" | cut -f1 )
 	amountFiles=$( ls "$i" | wc -l )
-	echo "Directory: $i Size: $amountSize Value of files:$amountFiles"
+	echo "Directory: $i Size: $amountSize Value of files:$amountFiles" >> "$2"
 
 done
 
