26ef3c84fbf6767bfd2186e53b4aa7a08b013fc0
Sawer-Grin <68555428+Sawer-Grin@users.noreply.github.com>
2022-02-16 22:34:32 +0300

    Update laba1.sh
    close #1

--- laba1.sh
+++ laba1.sh
@@ -8,13 +8,21 @@ if [ "$#" != 2 ]; then
 	echo "second parametr - file for saving result"
 	exit 0
 fi 
-if [[ ! -d "$directory" || ! -f "$fileSave" ]]; then 
-	echo "you input uncorrect parametrs"
+if [[ ! -d "$directory" ]]; then 
+	echo "Problem with file for saving"	
+	echo "first parametr - directory for search"
 	exit 0
 fi
 
+if [[ ! -f "$fileSave" ]]; then 
+	echo "Problem with directory"	
+	echo "second parametr - file for saving result"
+	exit 0
+fi
+
+
 for i in $( find "$directory" -type d); do
-	amountSize=$( du --max-depth=0 --apparent-size "$i" | cut -f1 )
+	amountSize=$( du -b -h --summarize "$i" | cut -f1 )
 	amountFiles=$( find "$i" -maxdepth 1 -type f | wc -l )
 	echo "Directory: $i Size: $amountSize Value: $amountFiles" >> "$fileSave"
 
